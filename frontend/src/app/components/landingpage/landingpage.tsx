e1dc2c9e33c39d6fc82cbe7da7f9cb9ab853c40a
Kaushik H S <kaushik0h0s@gmail.com>
2025-09-17 15:07:21 +0530

    adding landing page

--- frontend/src/app/components/landingpage/landingpage.tsx
+++ frontend/src/app/components/landingpage/landingpage.tsx
@@ -1,12 +1,39 @@
+'use client'
 import React from 'react'
+import { motion, useScroll, useTransform } from "framer-motion"
 
-const landingpage = () => {
+const LandingPage = () => {
+  const { scrollYProgress } = useScroll();
+  
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  
   return (
     <div>
-      <img className='w-full h-full ' src='/heroKeyArt.webp' alt="Hero Key Art"/>
-      <img className='w-full h-full absolute top-0 right-0' src='/heroKeyArt logo.webp' alt="Hero Key Art"/>'
+    <motion.div
+      style={{
+        scale,
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <img 
+        className='w-full h-full object-cover' 
+        src='/hmm.png' 
+        alt="Hero Key Art"
+      />
+      <img 
+        className='w-full h-full absolute top-0 right-0 object-cover' 
+        src='/heroKeyArt logo.webp' 
+        alt="Hero Key Art"
+      />
+    </motion.div>
+
     </div>
   )
 }
 
-export default landingpage
+export default LandingPage
